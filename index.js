a550578e3795d39f72f025508f1fadf3c460219d
Jay Jackson <jaynickjackson@gmail.com>
2018-01-18 12:13:09 -0500

    added planet table

--- index.js
+++ index.js
@@ -2,22 +2,53 @@ $(() => {
 
   let loaded = false
 
-  $('#infoButton').click(() => {
+  $('#clearButton').click(() => {
+    $('#tableBody').empty()
+    $('#dataOne').text('Pick')
+    $('#dataTwo').text('From')
+    $('#dataThree').text('Above')
+    loaded = false
+  })
+
+  $('#peopleButton').click(() => {
     if(!loaded) {
+      loaded = true
+
       $.ajax({
         type: 'GET',
         url: 'https://swapi.co/api/people'
       }).done((res) => {
+        $('#dataOne').text('Name')
+        $('#dataTwo').text('Height')
+        $('#dataThree').text('Birth Year')
+
         let people = res.results
         for(p of people) {
-          $('#tableBody').append(createTableRow(p))
+          $('#tableBody').append(createPersonRow(p))
         }
-        loaded = true
       })
     }
   })
 
-  function createTableRow(person) {
+  $('#planetButton').click(() => {
+    if(!loaded){
+      loaded = true
+
+      $.get('https://swapi.co/api/planets/')
+        .done((res) => {
+          $('#dataOne').text('Name')
+          $('#dataTwo').text('Diameter')
+          $('#dataThree').text('Climate')
+
+          let planets = res.results
+          for (p of planets) {
+            $('#tableBody').append(createPlanetRow(p))
+          }
+        })
+    }
+  })
+
+  function createPersonRow(person) {
     let row = $(`<tr></tr>`)
     let name = $(`<td>${person.name}</td>`)
     let height = $(`<td>${person.height}</td>`)
@@ -30,4 +61,15 @@ $(() => {
     return row
   }
 
+  function createPlanetRow(planet) {
+    let row = $(`<tr></tr>`)
+    let name = $(`<td>${planet.name}</td>`)
+    let diameter = $(`<td>${planet.diameter}</td>`)
+    let climate = $(`<td>${planet.climate}</td>`)
+
+    row.append(name, diameter, climate)
+
+    return row
+  }
+  
 })

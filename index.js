5e39b254567f60fcb14a7b1efcff90efdba5cefa
Jay Jackson <jaynickjackson@gmail.com>
2018-01-18 10:39:01 -0500

    added table population

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 $(() => {
 
+  let loaded = false
+
   $('#infoButton').click(() => {
-    $.ajax({
-      type: 'GET',
-      url: 'https://swapi.co/api/people'
-    }).done((res) => {
-      let people = res.results
-      for(person of people) {
-        console.log(person.name)
-      }
-    })
+    if(!loaded) {
+      $.ajax({
+        type: 'GET',
+        url: 'https://swapi.co/api/people'
+      }).done((res) => {
+        let people = res.results
+        for(p of people) {
+          $('#tableBody').append(createTableRow(p))
+        }
+        loaded = true
+      })
+    }
   })
+
+  function createTableRow(person) {
+    let row = $(`<tr></tr>`)
+    let name = $(`<td>${person.name}</td>`)
+    let height = $(`<td>${person.height}</td>`)
+    let birth = $(`<td>${person.birth_year}</td>`)
+
+    row.append(name)
+    row.append(height)
+    row.append(birth)
+
+    return row
+  }
 
 })
